790577b9ca7c94d92f21608e8f81bcde7e8a8a92
Lucio Alconchel <lucioalconchelsj@gmail.com>
2022-12-26 22:29:02 -0300

    Upload de Eliminacion de Tablas e Incersion de datos

--- DDL_Primera-Etapa.sql
+++ DDL_Primera-Etapa.sql
@@ -6,28 +6,18 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE reclamo (
-   claim_id int NOT NULL auto_increment PRIMARY KEY,
-   email_user VARCHAR(50) NOT NULL,
+    claim_id int NOT NULL auto_increment PRIMARY KEY,
+    email_user VARCHAR(50) NOT NULL,
     date DATE,
     description VARCHAR(200) NOT NULL,
     FOREIGN KEY (email_user) REFERENCES usuario(email)
 );
 
-CREATE TABLE descuento(
-   discount_id int NOT NULL PRIMARY KEY ,
-    email_user VARCHAR(50) NOT NULL,
-    discount_name VARCHAR(30),
-    type_service VARCHAR(30) NOT NULL,
-    expiration_date date NOT NULL,
-    description VARCHAR(200) NOT NULL,
-    FOREIGN KEY (email_user) REFERENCES usuario (email)
-);
-
 CREATE TABLE pago(
-   payment_code VARCHAR(20) NOT NULL PRIMARY KEY,
+    payment_code VARCHAR(20) NOT NULL PRIMARY KEY,
     email_user VARCHAR(50) NOT NULL,
     payment_method VARCHAR(30) NOT NULL,
-    bank_name VARCHAR(30) NOT NULL,
+    bank_name VARCHAR(50) ,
     service_name  VARCHAR(30) NOT NULL,
     FOREIGN KEY (email_user) REFERENCES usuario (email)
 );
@@ -38,18 +28,26 @@ CREATE TABLE sistema_operativo(
 );
 
 CREATE TABLE plataforma(
-    plataform VARCHAR(30)  PRIMARY KEY,
+    plataform VARCHAR(30) NOT NULL ,
     email_user VARCHAR(50) NOT NULL,
-    so_name VARCHAR(20),
+    so_name VARCHAR(20) NOT NULL,
     resolution VARCHAR(10),
     FOREIGN KEY (so_name) REFERENCES sistema_operativo (so_name),
     FOREIGN KEY (email_user) REFERENCES usuario (email)
 );
 
 CREATE TABLE servicio(
-   service_name VARCHAR(30),
+	service_name VARCHAR(30) PRIMARY KEY NOT NULL,
     type_service VARCHAR(30),
-    so_name VARCHAR(20),
-    FOREIGN KEY (service_name) REFERENCES plataforma(plataform),
-    FOREIGN KEY (so_name) REFERENCES sistema_operativo (so_name)
+    plataform VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE descuento(
+   discount_id int NOT NULL PRIMARY KEY ,
+    email_user VARCHAR(50) NOT NULL,
+    discount_name VARCHAR(30),
+    service_name VARCHAR(30) NOT NULL,
+    expiration_date date NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    FOREIGN KEY (email_user) REFERENCES usuario (email)
 );
---
+++ Insercion_Datos.sql
@@ -0,0 +1,88 @@
+-- TABLA 1
+INSERT INTO usuario VALUES
+    ('rosalyn_matthams@gmail.com', 'Rosalyn','Matthams',3),
+    ('miguel_boyce@gmail.com','Miguel','Boyce',1),
+    ('refugio_reynolds@outlook.com','Refugio','Reynolds',5),
+    ('hilde_milne@protonmail.com','Hilde','Milene',4),
+    ('ardell_samuels@mail.com','Ardell','Samuels',7),
+    ('lissette_silva@yandex.com','Lissette','Silva',2),
+    ('tasia_lloyd@hotmail.com','Tasia','Lloyd',3),
+    ('albertina_redman@yahoo.com','Albertina','Redman',1),
+    ('levi_west@yandex.com','Levi','West',5),
+    ('kelli_brett@protonmail.com','Kelli','Brett',7),
+    ('maynard_penn@aol.com','Maynard','Penn',6)
+    ;
+-- TABLA 2
+INSERT INTO reclamo VALUES
+    (1,'maynard_penn@aol.com','2021-11-16','No se acredito mi suscripcion'),
+    (2,'kelli_brett@protonmail.com','2022-12-23','Caidas intermitentes en el servicio'),
+    (3,'hilde_milne@protonmail.com','2022-11-24','No es lo que esperaba'),
+    (4,'hilde_milne@protonmail.com','2022-07-06','Catalogo reducido y no cuenta con proteccion para menores')
+    ;
+-- TABLA 3
+INSERT INTO pago VALUES
+    ('5HO85','rosalyn_matthams@gmail.com','Tarjeta','S.A. Banco Santander','Streaming'),
+    ('MAR0X','miguel_boyce@gmail.com','Tarjeta','BANCO SUPERVIELLE S.A.','Streaming'),
+    ('202WI','refugio_reynolds@outlook.com','Deposito','NULL','Nube'),
+    ('ZKHYR','hilde_milne@protonmail.com','Deposito','NULL','Nube'),
+    ('V7VUL','ardell_samuels@mail.com','Deposito','NULL','Educacion'),
+    ('L23E6','lissette_silva@yandex.com','Tarjeta','BANCO SUPERVIELLE S.A.','Nube'),
+    ('PXO5B','tasia_lloyd@hotmail.com','Tarjeta','BANCO DE LA CIUDAD DE BUENOS AIRES','Educacion'),
+    ('1ZJH6','albertina_redman@yahoo.com','Tarjeta','BANCO BBVA ARGENTINA S.A.','Nube'),
+    ('22N18','levi_west@yandex.com','Deposito','NULL','Streaming'),
+    ('L9C39','kelli_brett@protonmail.com','Deposito','NULL','Educacion'),
+    ('AMJB5','maynard_penn@aol.com','Tarjeta','BANCO BBVA ARGENTINA S.A.','Streaming')
+    ;
+-- TABLA 4
+INSERT INTO sistema_operativo VALUES
+    ('Windows_8.1','6.3'),
+    ('Windows_10','22h2'),
+    ('Windows_11','22h2'),
+    ('MAC_OS_Mojave','10.14'),
+    ('MAC_OS_Catalina','10.15'),
+    ('Android_11','11'),
+    ('Android_12L','12.1'),
+    ('Android_13','13'),
+    ('¡OS_12','12.5.5'),
+    ('¡OS_13','13.7')
+    ;
+-- TABLA 5
+INSERT INTO servicio VALUES
+    ('Platzi','Educacion','Web'),
+    ('Udemy','Educacion','Web'),
+    ('YoutubeMusic','Streaming','Web'),
+    ('Spotify','Streaming','Web, Pc, Movil, Tv'),
+    ('Netflix','Streaming','Web, Pc, Movil, Tv'),
+    ('Dropbox','Nube','Web, Pc, Movil'),
+    ('Drive','Nube','Web, Movil'),
+    ('Mega','Nube','Web, Movil, Pc'),
+    ('SoundCloud','Streaming','Web'),
+    ('HBOMAX','Streaming','Web, Movil, Tv')
+    ;
+-- TABLA 6
+INSERT INTO plataforma VALUES
+    ('Movil','rosalyn_matthams@gmail.com','¡OS_13','720X1280'),
+    ('Movil','miguel_boyce@gmail.com','Android_12L','720X1280'),
+    ('Web','refugio_reynolds@outlook.com','MAC_OS_Mojave','NULL'),
+    ('Tv','hilde_milne@protonmail.com','Android_13','3840X2160'),
+    ('Movil','ardell_samuels@mail.com','Android_11','720X1280'),
+    ('Tv','lissette_silva@yandex.com','Android_12L','3840X2160'),
+    ('Movil','tasia_lloyd@hotmail.com','¡OS_12','720X1280'),
+    ('Pc','albertina_redman@yahoo.com','Windows_11','1920X1080'),
+    ('Pc','levi_west@yandex.com','MAC_OS_Catalina','1920X1080'),
+    ('Pc','kelli_brett@protonmail.com','Windows_11','1920X1080'),
+    ('Tv','maynard_penn@aol.com','Android_12L','3840X2160')
+    ;
+-- TABLA 7
+INSERT INTO descuento VALUES
+    (1,'maynard_penn@aol.com','REINTEGRO35%','Platzi','2022-12-04','35% reintegro con la suscripcion de un anio'),
+    (2,'kelli_brett@protonmail.com','50%OFF','Souncloud','2022-01-04','50% OFF en el segundo mes'),
+    (3,'hilde_milne@protonmail.com','25%DESCUENTO','Drive','2022-12-02','25% descuento'),
+    (4,'hilde_milne@protonmail.com','MESGRATIS','YoutubeMusic','2021-02-18','Primer mes GRATIS'),
+    (5,'levi_west@yandex.com','MESESPRUEBA','HBOMAX','2022-09-27','Tres meses de PRUEBA'),
+    (6,'miguel_boyce@gmail.com','50%OFFEN1ANIO','Udemy','2022-11-26','50% OFF pagando un anio'),
+    (7,'lissette_silva@yandex.com','HBOMAXGRATIS','HBOMAX','2022-02-12','Suscripcion gratis en HBO MAX por un mes'),
+    (8,'levi_west@yandex.com','REINTEGRO40%','Dropbox','2022-05-22','Reintegro del 40% de tu suscripcion'),
+    (9,'refugio_reynolds@outlook.com','10%DESCUENTO','Mega','2022-01-16','10% de descuento'),
+    (10,'rosalyn_matthams@gmail.com','65%OFF','Spotify','2021-03-25','65% OFF en seis meses')
+    ;
